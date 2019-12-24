cfa01af4ad4201fd73d804eea3545999c7636614
otomer <o.tomerr@gmail.com>
2019-12-24 15:45:19 +0200

    .

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const cloneArray = arr => [].concat(arr);
-
-const createSequence = (len, from = 0) =>
-  Array.from({ length: len }, (value, key) => key + from);
-
-const chunkArray = (arr, chunkSize) => {
-  var index = 0;
-  var arrayLength = arr.length;
-  var tempArray = [];
-
-  for (index = 0; index < arrayLength; index += chunkSize) {
-    chunk = arr.slice(index, index + chunkSize);
-    tempArray.push(chunk);
-  }
-
-  return tempArray;
-};
-
-const sortByAttribute = (arr, attr, asc = true) =>
-  arr.sort((a, b) => (a[attr] > b[attr] ? (asc ? -1 : 1) : asc ? 1 : -1));
+const {
+  cloneArray,
+  createSequence,
+  chunkArray,
+  sortByAttribute
+} = require("algoritool");
 
 // ****************
 // *** Exported ***
@@ -78,11 +64,11 @@ const generateString = () =>
     .substring(2, 15);
 
 module.exports = {
-  shuffle: shuffle,
-  pickNumbers: pickNumbers,
-  pickHexColor: pickHexColor,
-  priorityGroups: priorityGroups,
-  groups: groups,
-  random: random,
-  generateString: generateString
+  shuffle,
+  pickNumbers,
+  pickHexColor,
+  priorityGroups,
+  groups,
+  random,
+  generateString
 };
